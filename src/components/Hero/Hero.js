be923d414dd5374667c9b4f5a8d16217c4559c3d
Preston C <pchandler858@gmail.com>
2023-06-13 14:59:57 -0700

    nav bar and hero styling

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -3,14 +3,25 @@ import "./Hero.module.css"; // Assuming you have a CSS file for styling the Hero
 
 const Hero = () => {
   return (
-    <section className="hero">
-      <h1 className="hero-title">
-        Hi, My name is Preston Chandler. I'm the Developer you've been looking
-        for.
-      </h1>
-      <a href="#contact" className="hero-button">
-        Contact Me
-      </a>{" "}
+    <section
+      id="hero"
+      className=" hero jumbotron min-h-screen flex items-center"
+    >
+      <div className="container mx-auto text-center py-4">
+        <h1 className="hero-title text-4xl font-bold leading-tight">
+          Hi, My name is
+          <span className="bg-clip-text gradient-text">Preston Chandler.</span>
+          <br />
+          I'm the Developer you've been looking for.
+        </h1>
+        <a href="#about">
+          <button className="btn btn-white">
+            <span className="gradient-text" id="btn-white">
+              Learn Why
+            </span>
+          </button>
+        </a>
+      </div>
     </section>
   );
 };
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -2,19 +2,39 @@ import React from "react";
 
 function Navigation() {
   return (
-    <nav>
-      <ul>
-        <li>
-          <a href="#about">About Me</a>
+    <nav className="flex justify-end items-center py-3 bg-transparent font-semibold">
+      <ul className="flex">
+        <li className="nav-item">
+          <a
+            className="nav-link px-4 text-black hover:text-blue-600"
+            href="#about"
+          >
+            ABOUT ME
+          </a>
         </li>
-        <li>
-          <a href="#work">Projects</a>
+        <li className="nav-item">
+          <a
+            className="nav-link px-4 text-black hover:text-blue-600"
+            href="#projects"
+          >
+            PROJECTS
+          </a>
         </li>
-        <li>
-          <a href="#contact">Contact</a>
+        <li className="nav-item">
+          <a
+            className="nav-link px-4 text-black hover:text-blue-600"
+            href="#contact"
+          >
+            CONTACT
+          </a>
         </li>
-        <li>
-          <a href="#resume">Resume</a>
+        <li className="nav-item">
+          <a
+            className="nav-link px-4 text-black hover:text-blue-600"
+            href="#resume"
+          >
+            RESUME
+          </a>
         </li>
       </ul>
     </nav>
