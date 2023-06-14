91eae59e1c6b1d7613ee4fb7fda98a4e31f97798
Preston C <pchandler858@gmail.com>
2023-06-14 14:10:37 -0700

    contact page meets all criteria

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,20 +7,51 @@ function Contact() {
     message: "",
   });
 
+  const [errors, setErrors] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const [formSubmitted, setFormSubmitted] = useState(false);
+
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
+    setFormSubmitted(false);
+  };
+
+  const handleBlur = (e) => {
+    const { name, value } = e.target;
+    let tempErrors = { ...errors };
+
+    if (!value) {
+      tempErrors[name] = "This field is required";
+    } else {
+      tempErrors[name] = "";
+    }
+
+    if (name === "email" && value) {
+      const emailRegex = /^\S+@\S+\.\S+$/;
+      if (!emailRegex.test(value)) {
+        tempErrors[name] = "Please enter a valid email address";
+      }
+    }
+
+    setErrors(tempErrors);
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(formData);
+    setFormData({ name: "", email: "", message: "" });
+    setFormSubmitted(true);
   };
 
   return (
     <section id="contact" className="about">
       <div className="w-full px-4">
         <h2 className="section-title">GET IN TOUCH</h2>
-        <p class="pb-10 px-10 text-left ">
+        <p className="pb-10 px-10 text-left ">
           As a highly skilled and motivated web developer, I am actively seeking
           new employment opportunities where I can utilize my expertise to
           deliver outstanding results. I am always eager to take on new
@@ -28,47 +59,56 @@ function Contact() {
           looking for a talented web developer to join your team, I would be
           honored to discuss how I can contribute to your success.
         </p>
-        <form
-          id="contact-form"
-          onSubmit={handleSubmit}
-          className="space-y-6 shadow-md rounded px-8 pt-6 pb-8 mb-4 bg-slate-300 text-black max-w-xl mx-auto"
-        >
-          <label className="block">
-            <span className="">Name:</span>
-            <input
-              type="text"
-              name="name"
-              onChange={handleChange}
-              value={formData.name}
-              required
-              className="mt-1 p-1 block w-full rounded-sm border border-slate-500 shadow-sm focus:border-blue-500 focus:ring-blue-500 text-slate-900"
-            />
-          </label>
-          <label className="block">
-            <span className="">Email:</span>
-            <input
-              type="email"
-              name="email"
-              onChange={handleChange}
-              value={formData.email}
-              required
-              className="mt-1 p-1 block w-full rounded-sm border border-slate-500 shadow-sm focus:border-blue-500 focus:ring-blue-500 text-slate-900"
-            />
-          </label>
-          <label className="block">
-            <span className="">Message:</span>
-            <textarea
-              name="message"
-              onChange={handleChange}
-              value={formData.message}
-              required
-              className="h-32 mt-1 block w-full rounded-sm border border-slate-500 shadow-sm focus:border-blue-500 focus:ring-blue-500"
-            />
-          </label>
-        </form>
-        <button type="submit" className="btn btn-blue px-4 py-2">
-          Submit
-        </button>
+        <div className="">
+          <form
+            id="contact-form"
+            onSubmit={handleSubmit}
+            className="space-y-6 shadow-md rounded px-8 pt-6 pb-8 mb-4 bg-slate-300 text-black max-w-xl mx-auto"
+          >
+            <label className="block">
+              <span className="">Name:</span>
+              <input
+                type="text"
+                name="name"
+                onChange={handleChange}
+                value={formData.name}
+                required
+                onBlur={handleBlur}
+                className="mt-1 p-1 block w-full rounded-sm border border-slate-500 shadow-sm focus:border-blue-500 focus:ring-blue-500 text-slate-900"
+              />
+              {errors.name && <p className="error">{errors.name}</p>}
+            </label>
+            <label className="block">
+              <span className="">Email:</span>
+              <input
+                type="email"
+                name="email"
+                onChange={handleChange}
+                value={formData.email}
+                required
+                onBlur={handleBlur}
+                className="mt-1 p-1 block w-full rounded-sm border border-slate-500 shadow-sm focus:border-blue-500 focus:ring-blue-500 text-slate-900"
+              />
+              {errors.email && <p className="error">{errors.email}</p>}
+            </label>
+            <label className="block">
+              <span className="">Message:</span>
+              <textarea
+                name="message"
+                onChange={handleChange}
+                value={formData.message}
+                required
+                onBlur={handleBlur}
+                className="h-32 mt-1 block w-full rounded-sm border border-slate-500 shadow-sm focus:border-blue-500 focus:ring-blue-500"
+              />
+              {errors.message && <p className="error">{errors.message}</p>}
+              {formSubmitted && <p className="pt-5">Thank you! Your message has been sent!</p>}
+            </label>
+            <button type="submit" className="btn btn-blue px-4 py-2">
+              Submit
+            </button>
+          </form>
+        </div>
       </div>
     </section>
   );
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,36 +3,36 @@ import { Link } from "react-scroll";
 
 const Footer = () => {
   return (
-    <footer class=" pt-10 text-center">
-      <div class="">
+    <footer className=" pt-10 text-center">
+      <div className="">
         <Link to="top" spy={true} smooth={true} offset={0} duration={1000}>
-          <i class="fa fa-angle-up fa-2x" aria-hidden="true"></i>
+          <i className="fa fa-angle-up fa-2x" aria-hidden="true"></i>
         </Link>
-        <div class="social-links mb-4">
+        <div className="social-links mb-4">
           <a
             rel="noreferrer"
             href="https://twitter.com/PrestonChandler"
             target="_blank"
           >
-            <i class="fa-brands fa-twitter fa-2x"></i>
+            <i className="fa-brands fa-twitter fa-2x"></i>
           </a>
           <a
             rel="noreferrer"
             href="https://www.linkedin.com/in/preston-chandler/"
             target="_blank"
           >
-            <i class="fa-brands fa-linkedin fa-2x"></i>
+            <i className="fa-brands fa-linkedin fa-2x"></i>
           </a>
           <a
             rel="noreferrer"
             href="https://github.com/pchandler858"
             target="_blank"
           >
-            <i class="fa-brands fa-github fa-2x"></i>
+            <i className="fa-brands fa-github fa-2x"></i>
           </a>
         </div>
         <hr />
-        <p class="footer my-5">
+        <p className="footer my-5">
           © 2023 - {""}
           <a
             rel="noreferrer"
