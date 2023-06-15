6324ae3e22dcd2d1d9cc24117e070920517bbd86
Preston C <pchandler858@gmail.com>
2023-06-15 13:51:53 -0700

    navbar responsive

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,23 +1,113 @@
-// import React, { useState } from "react";
+// // import React, { useState } from "react";
+// import { Link } from "react-scroll";
+
+// function Navigation({ currentPage, handlePageChange }) {
+//   return (
+//     <nav className="flex justify-between items-center py-3 font-semibold">
+//       <div className="ml-4">
+//         <span className="text-slate-900">PRESTON CHANDLER</span>
+//       </div>
+//       <ul className="flex">
+//         <li className="nav-item">
+//           <Link
+//             activeClass="text-slate-500 underline"
+//             // to="about"
+//             // spy={true}
+//             // smooth={true}
+//             // offset={0}
+//             // duration={800}
+//             onClick={() => handlePageChange("About")}
+//             className={`nav-link px-4  hover:text-slate-500 hover:cursor-pointer ${
+//               currentPage === "About"
+//                 ? "text-slate-500 underline"
+//                 : "text-slate-900"
+//             }`}
+//           >
+//             ABOUT ME
+//           </Link>
+//         </li>
+//         <li className="nav-item">
+//           <Link
+//             activeClass="text-slate-500 underline"
+//             // to="projects"
+//             // spy={true}
+//             // smooth={true}
+//             // offset={0}
+//             // duration={800}
+//             onClick={() => handlePageChange("Projects")}
+//             className={`nav-link px-4 hover:text-slate-500 hover:cursor-pointer ${
+//               currentPage === "Projects"
+//                 ? "text-slate-500 underline"
+//                 : "text-slate-900"
+//             }`}
+//           >
+//             PORTFOLIO
+//           </Link>
+//         </li>
+//         <li className="nav-item">
+//           <Link
+//             activeClass="text-slate-500 underline"
+//             // to="contact"
+//             // spy={true}
+//             // smooth={true}
+//             // offset={0}
+//             // duration={800}
+//             onClick={() => handlePageChange("Contact")}
+//             className={`nav-link px-4  hover:text-slate-500 hover:cursor-pointer ${
+//               currentPage === "Contact"
+//                 ? "text-slate-500 underline"
+//                 : "text-slate-900"
+//             }`}
+//           >
+//             CONTACT
+//           </Link>
+//         </li>
+//         <li className="nav-item">
+//           <Link
+//             activeClass="text-slate-500 underline"
+//             // to="resume"
+//             // spy={true}
+//             // smooth={true}
+//             // offset={0}
+//             // duration={800}
+//             onClick={() => handlePageChange("Resume")}
+//             className={`nav-link px-4  hover:text-slate-500 hover:cursor-pointer ${
+//               currentPage === "Resume"
+//                 ? "text-slate-500 underline"
+//                 : "text-slate-900"
+//             }`}
+//           >
+//             RESUME
+//           </Link>
+//         </li>
+//       </ul>
+//     </nav>
+//   );
+// }
+
+// export default Navigation;
+
+import React from "react";
 import { Link } from "react-scroll";
 
 function Navigation({ currentPage, handlePageChange }) {
   return (
     <nav className="flex justify-between items-center py-3 font-semibold">
       <div className="ml-4">
-        <span className="text-slate-900">PRESTON CHANDLER</span>
+        <span className="text-slate-900">PRESTON C.</span>
       </div>
-      <ul className="flex">
+
+      <ul className="flex flex-col sm:flex-row">
         <li className="nav-item">
           <Link
             activeClass="text-slate-500 underline"
-            // to="about"
-            // spy={true}
-            // smooth={true}
-            // offset={0}
-            // duration={800}
+            to="about"
+            spy={true}
+            smooth={true}
+            offset={0}
+            duration={800}
             onClick={() => handlePageChange("About")}
-            className={`nav-link px-4  hover:text-slate-500 hover:cursor-pointer ${
+            className={`nav-link px-4 hover:text-slate-500 hover:cursor-pointer ${
               currentPage === "About"
                 ? "text-slate-500 underline"
                 : "text-slate-900"
@@ -29,11 +119,11 @@ function Navigation({ currentPage, handlePageChange }) {
         <li className="nav-item">
           <Link
             activeClass="text-slate-500 underline"
-            // to="projects"
-            // spy={true}
-            // smooth={true}
-            // offset={0}
-            // duration={800}
+            to="projects"
+            spy={true}
+            smooth={true}
+            offset={0}
+            duration={800}
             onClick={() => handlePageChange("Projects")}
             className={`nav-link px-4 hover:text-slate-500 hover:cursor-pointer ${
               currentPage === "Projects"
@@ -47,13 +137,13 @@ function Navigation({ currentPage, handlePageChange }) {
         <li className="nav-item">
           <Link
             activeClass="text-slate-500 underline"
-            // to="contact"
-            // spy={true}
-            // smooth={true}
-            // offset={0}
-            // duration={800}
+            to="contact"
+            spy={true}
+            smooth={true}
+            offset={0}
+            duration={800}
             onClick={() => handlePageChange("Contact")}
-            className={`nav-link px-4  hover:text-slate-500 hover:cursor-pointer ${
+            className={`nav-link px-4 hover:text-slate-500 hover:cursor-pointer ${
               currentPage === "Contact"
                 ? "text-slate-500 underline"
                 : "text-slate-900"
@@ -65,13 +155,13 @@ function Navigation({ currentPage, handlePageChange }) {
         <li className="nav-item">
           <Link
             activeClass="text-slate-500 underline"
-            // to="resume"
-            // spy={true}
-            // smooth={true}
-            // offset={0}
-            // duration={800}
+            to="resume"
+            spy={true}
+            smooth={true}
+            offset={0}
+            duration={800}
             onClick={() => handlePageChange("Resume")}
-            className={`nav-link px-4  hover:text-slate-500 hover:cursor-pointer ${
+            className={`nav-link px-4 hover:text-slate-500 hover:cursor-pointer ${
               currentPage === "Resume"
                 ? "text-slate-500 underline"
                 : "text-slate-900"
